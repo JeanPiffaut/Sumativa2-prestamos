1f23ec74d526bdce0e07d89f981623494a7ca71f
Erick-Caro <erick.caro.h@gmail.com>
2023-04-23 18:49:27 -0400

    Update Usuario.java

--- src/main/java/com/mycompany/applibreria/Usuario.java
+++ src/main/java/com/mycompany/applibreria/Usuario.java
@@ -4,6 +4,9 @@
  */
 package com.mycompany.applibreria;
 
+//import java.io.Console;
+import java.util.ArrayList;
+
 /**
  *
  * @author Tom
@@ -11,6 +14,8 @@ package com.mycompany.applibreria;
 public abstract class Usuario {
     private String RUN;
     private String nombreCompleto;
+    private char Genero;
+    private String carrera;
     private boolean conPrestamo;
     
     public Usuario(String RUN, String nombreCompleto, boolean conPrestamo) {
@@ -50,7 +55,7 @@ public abstract class Usuario {
     /**
      * @return the conPrestamo
      */
-    public boolean isConPrestamo() {
+    public boolean getConPrestamo() {
         return conPrestamo;
     }
 
@@ -59,12 +64,184 @@ public abstract class Usuario {
      */
     public void setConPrestamo(boolean conPrestamo) {
         this.conPrestamo = conPrestamo;
+    } 
+
+    /**
+     * @return the Genero
+     */
+    public char getGenero() {
+        return Genero;
+    }
+
+    /**
+     * @param Genero the Genero to set
+     */
+    public void setGenero(char Genero) {
+        this.Genero = Genero;
+    }
+    
+        /**
+     * @return the carrera
+     */
+    public String getCarrera() {
+        return carrera;
+    }
+
+    /**
+     * @param carrera the carrera to set
+     */
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+    
+    public static boolean crearUsuario(Usuario user, ArrayList<Usuario> usuarios)
+    {
+        try{
+            
+        for(int i =0; i < usuarios.size(); i++)
+        {
+            Usuario usuario = usuarios.get(i);
+            
+            if(usuario.getRUN().equals(user.getRUN()))
+            {
+                System.out.println("El rut ingresado ya existe");
+                return false;
+            }
+            
+            boolean validaRut = validarRut(user.getRUN());
+            if(validaRut == false)
+            {
+                System.out.println("el rut ingresado no es valido");
+                return false;
+            }
+            
+            if(user.getGenero() != 'M' || user.getGenero() != 'F')
+            {
+                System.out.println("El genero ingresado no es correcto");
+                     
+                return false;
+            }
+            
+            if(user.getConPrestamo() == true)
+            {
+                System.out.println("El genero ingresado no es correcto");
+                return false;
+            }
+        }
+       
+        
+        usuarios.add(user);
+        System.out.println("Usuario agregado correctamente");
+        return true;
+        
+        }
+        catch(Exception e)
+        {
+            throw new IllegalArgumentException("El usuario no se ha ingresado");
+        }
+    }
+    
+    public static boolean editarUsuario(Usuario user, ArrayList<Usuario> usuarios)
+    {
+        try{
+            
+        for(int i =0; i < usuarios.size(); i++)
+        {
+            Usuario usuario = usuarios.get(i);
+            
+            if(usuario.getRUN().equals(user.getRUN()))
+            {
+                 boolean validaRut = validarRut(user.getRUN());
+            if(validaRut == false)
+            {
+                System.out.println("el rut ingresado no es valido");
+                return false;
+            }
+            
+            if(user.getGenero() != 'M' || user.getGenero() != 'F')
+            {
+                System.out.println("El genero ingresado no es correcto");
+                     
+                return false;
+            }
+            
+            if(user.getConPrestamo() == true)
+            {
+                System.out.println("El genero ingresado no es correcto");
+                return false;
+            }
+            
+                usuario.Genero = user.getGenero();
+                usuario.nombreCompleto = user.getNombreCompleto();
+                usuario.carrera = user.getCarrera();
+                usuario.conPrestamo = user.getConPrestamo();
+                System.out.println("Usuario modificado correctamente");
+                return true;
+            }                  
+        }
+            return true;
+        }
+        catch(Exception e)
+        {
+            throw new IllegalArgumentException("El usuario no se ha ingresado");
+        }
+    }
+        
+    public static boolean eliminarUsuario(String rut, ArrayList<Usuario> usuarios)
+    {
+        try{
+            
+            for(int i =0; i < usuarios.size(); i++)
+            {
+                Usuario usuario = usuarios.get(i);
+                
+                if(usuario.getRUN().equals(rut))
+                {
+                    usuarios.remove(i);
+                    System.out.println("Usuario eliminado correctamente");
+                    return true;
+                }                
+            }
+            return false;
+        }
+        catch(Exception e)
+        {
+            throw new IllegalArgumentException("El usuario no se ha eliminado");
+        }        
+    }
+    
+    public static boolean validarRut(String cedula)
+    {
+        boolean validacion = false;
+        try {
+            String rut =  cedula.toUpperCase();
+            rut = rut.replace(".", "");
+            rut = rut.replace("-", "");
+            int rutAux = Integer.parseInt(rut.substring(0, rut.length() - 1));
+
+            char dv = rut.charAt(rut.length() - 1);
+
+            int m = 0, s = 1;
+            for (; rutAux != 0; rutAux /= 10) {
+                s = (s + rutAux % 10 * (9 - m++ % 6)) % 11;
+            }
+            if (dv == (char) (s != 0 ? s + 47 : 75)) {
+                validacion = true;
+            }
+
+        } catch (java.lang.NumberFormatException e) {
+            throw new IllegalArgumentException("El rut ingresado no es valido.");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("El rut ingresado no es valido.");
+        }
+        return validacion;
     }
     
     public String toCSV() {
         // AGREGAR LOS ATRIBUTOS ADICIONALES SEPARADOS POR ;
         // 1-1;Tomás S;1
         
-        return getRUN() + ";" + getNombreCompleto();
+        return getRUN() + ";" + getNombreCompleto() + ";" + getCarrera()+ ";" + getGenero() + ";" + getConPrestamo() ;
     }
+
 }
